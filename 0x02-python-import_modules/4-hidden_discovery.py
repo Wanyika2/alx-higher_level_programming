d15fdcf42da4f277311c1cbf496eefa8ae45577d
Wanyika2 <dorothywanyika4@gmail.com>
2024-01-04 12:53:36 +0300

    Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from hidden_4 import *
+    alif = dir()
+    for i in range(0, len(alif)):
+        if alif[i][:2] != "__":
+            print("{}".format(alif[i]))
