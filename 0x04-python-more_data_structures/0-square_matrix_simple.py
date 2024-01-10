94759f601990ebcb6bed4b71c7be1fa9377ce37d
Wanyika2 <dorothywanyika4@gmail.com>
2024-01-10 12:39:06 +0300

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,9 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [[x**2 for x in row] for row in matrix]
     return new_matrix
 
+
 if __name__ == "__main__":
+
     matrix = [
         [1, 2, 3],
         [4, 5, 6],
