f50ba99a6b6703f2cb403ae07ed3b1876e607b69
Wanyika2 <dorothywanyika4@gmail.com>
2024-01-10 12:44:34 +0300

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,8 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x**2 for x in row] for row in matrix]
-    return new_matrix
+    new_matrix = matrix.copy()
 
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-if __name__ == "__main__":
-
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    new_matrix = square_matrix_simple(matrix)
-    print(new_matrix)
-    print(matrix)
+    return (new_matrix)
